cc3da195a83a85a4d424b8368f35d7f3afb62b0c
usman-s-mahmood <usmanshahid027@outlook.com>
2025-11-12 01:08:55 +0500

    trying to push startup.sh

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Apply database migrations
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start Gunicorn server
+gunicorn RDRFM.wsgi:application \
+    --config gunicorn_config.py
